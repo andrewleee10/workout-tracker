a363aef2a8de043c84fd9644a880fc12fb804b7c
Andrew Lee <andrewleee10@gmail.com>
2021-04-06 10:20:52 -0700

    dot env

--- db/index.js
+++ db/index.js
@@ -1,1 +1,1 @@
-module.exports = require('mongoose').connect('mongodb://localhost/workout', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+module.exports = require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
